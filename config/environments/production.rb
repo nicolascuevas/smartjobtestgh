2f52fa5b0c08ff347ae041fb2630a14fd58f67f0
nicolascuevas <nicolascuevas@gmail.com>
2021-02-12 11:28:14 -0300

    testing domaing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,6 +111,7 @@ Rails.application.configure do
   # replica.
 
   config.hosts << "smartjobtest.herokuapp.com"
+  config.hosts << "testing.smartjob.us"
   #
   # By default Rails will store a last write timestamp in the session. The
   # DatabaseSelector middleware is designed as such you can define your own
